8fc332c00ea31f8ab43c8e0cfeb854a45b864c5b
marisantosteixeira <mariana.teixeira2@estudante.ifms.edu.br>
2023-11-20 11:32:53 -0400

    iniciando projeto

--- src/app/page.js
+++ src/app/page.js
@@ -1,60 +1,133 @@
 'use client'
-import { useState } from "react";
-import handlerAcessUser from "./functions/handlerAcess";
-import { useRouter } from "next/navigation";
-import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import "./global.css"
+import "./global.css";
+import 'react-toastify/dist/ReactToastify.css';
+import "./global.css";
 
-export default function Login() {
-  const [user, setUser] = useState({
-    email: '',
-    password: '',
-  });
- 
-  const { push, refresh} = useRouter();
 
-  const handlerLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const userAuth = await handlerAcessUser(user);
-      if(userAuth.token === undefined){
-        toast.error("Ocorreu um erro no email ou senha!")
-      }
-      push('/pages/dashboard');
-    } catch {
-      refresh(); //tualiza a página se ocorrer algum erro no try ou no catch
-    }
-  }
+export default function Home() {
   return (
-    <body>
-    <div class="body">
-      <form onSubmit={handlerLogin}>
-      <div>
-      </div>
-      <fieldset>
-      <h1>Login</h1>
+    <div>
+      <main>
+        <h1 >
+          Consciência Negra <img className="foto"src="https://ea9vhhuzko5.exactdn.com/wp-content/uploads/2015/11/consciencia.jpg?strip=all&lossy=1&quality=90&webp=90&avif=80&resize=509%2C720&ssl=1"/>
+        </h1>
 
-      <div className="abcd">
-        <input className="inputs"
-          placeholder='Insira seu E-mail'
-          type="email"
-          onChange={(e) => { setUser({ ...user, email: e.target.value }) }}>
-        </input>
+        <div>
+          <div>
+            <fieldset>
+            <h3>O que é consciência negra?</h3>
+            <p>A consciência negra é o reconhecimento e a valorização da história, cultura e identidade do povo negro no Brasil e no mundo. É também uma forma de resistir
+               e combater o racismo e a desigualdade que afetam a população negra em diversos aspectos. A consciência negra é um movimento social e cultural que busca afirmar a 
+               dignidade e o orgulho da negritude.</p>
+               </fieldset>
+          </div>
+
+          <div>
+          <fieldset>
+            <h3>Origem da consciência negra</h3>
+            <p>A consciência negra surgiu no Brasil na década de 1970, inspirada pelo movimento negro dos Estados Unidos, que lutava pelos direitos civis dos negros 
+              naquele país. No Brasil, o Movimento Negro Unido foi uma das principais organizações que difundiram a ideia de consciência negra e promoveram ações de
+               valorização da cultura afro-brasileira. O Dia da Consciência Negra é comemorado em 20 de novembro, data que homenageia Zumbi dos Palmares, 
+               um dos principais líderes da resistência negra contra a escravidão no Brasil. Zumbi foi morto em 20 de novembro de 1695, após liderar o Quilombo dos 
+               Palmares, uma comunidade formada por escravos fugidos que se tornou um símbolo de luta e liberdade.</p>
+               </fieldset>
+          </div>
+
+          <div>
+          <fieldset>
+            <h3>Por que é importante a consciência negra?</h3>
+            <p>A consciência negra é importante para conscientizar a sociedade sobre o papel dos negros na construção do Brasil e do mundo, bem como sobre os problemas 
+              que eles enfrentam, como o preconceito, a discriminação, a violência, a pobreza, a falta de oportunidades, entre outros. A consciência negra é também uma 
+              forma de celebrar a diversidade e a riqueza da cultura negra, que se manifesta na música, na arte, na religião, na gastronomia, na literatura, na ciência,
+               entre outras áreas.</p>
+               </fieldset>
+          </div>
         </div>
-        <div className="abcd">
-        <input className="inputs"
-          placeholder='Insira sua senha'
-          type='password'
-          onChange={(e) => { setUser({ ...user, password: e.target.value }) }}>
-        </input>
-        </div>
-        <br></br>
-        <button className="botao">Entrar</button>
-        </fieldset>
-      </form>
-      <ToastContainer/>
+        <h2>Pessoas negras que fizeram história pelo Brasil e pelo mundo</h2>
+        
+
+  <div>
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Rosaparks.jpg/200px-Rosaparks.jpg" />
+  Rosa Parks (1913-2005)
+  
+         
+         <p>   Rosa Parks foi uma ativista negra que lutou pelos direitos civis e contra a segregação racial nos Estados Unidos.
+             Ela ficou famosa por realizar um ato de desobediência civil quando se recusou a ceder seu assento a um homem branco 
+             em um ônibus público, no Alabama. A luta de Rosa Parks serviu de modelo para várias lideranças negras.</p>
+             </div>
+      
+      <div>
+  Martin Luther King Jr  (1929-1968) 
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Martin_Luther_King%2C_Jr..jpg/200px-Martin_Luther_King%2C_Jr..jpg"  />
+   
+         <p> Martin Luther King Jr. foi um grande líder pacifista. Lutou incessantemente pelos princípios de liberdade e igualdade, 
+          e pelos direitos civis na América. Pelo combate pacífico contra o preconceito racial, ganhou o Prêmio Nobel da Paz</p>
+          </div>
+
+          <div>
+Machado de Assis(1839-1908)
+
+        <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Machado_de_Assis_1904.jpg/250px-Machado_de_Assis_1904.jpg"/>
+    
+           <p> Machado de Assis foi o principal nome do Realismo brasileiro, o primeiro presidente da Academia Brasileira de Letras e um dos
+             escri
+          tores mais aclamados da literatura. O carioca nascido no Morro do Livramento atuou como jornalista, crítico, cronista, dramaturgo e
+           poeta.</p>
+     </div>
+
+     <div>
+    Djamila Ribeiro (1980)
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Djamila_Ribeiro.jpg/280px-Djamila_Ribeiro.jpg"></img>
+    <p>Djamila tornou-se conhecida no país por seu ativismo na internet, acumulando mais de 500 mil seguidores no Instagram. É colunista do 
+      jornal Folha de São Paulo e foi secretária-adjunta da Secretaria de Direitos Humanos e Cidadania de São Paulo.</p>
+      </div>
+
+     <div>
+
+  Marina Silva (1958)<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/%282022-09-14%29_Marina_Silva_5_%2852361148058%29_%28cropped_2%29.jpg/200px-%282022-09-14%29_Marina_Silva_5_%2852361148058%29_%28cropped_2%29.jpg" />
+        <p> Maria Osmarina Marina da Silva Vaz de Lima (nascida Maria Osmarina da Silva) (Rio Branco, 8 de fevereiro de 1958), mais conhecida
+           por Marina Silva, é uma historiadora, professora, psicopedagoga, ambientalista e política brasileira, filiada à Rede Sustentabilidade (REDE) e atual ministra do Meio Ambiente</p>
+           </div> 
+
+           <div>
+      Carolina Maria de Jesus (1914-1977)
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Carolina_Maria_de_Jesus_assinando_seu_livro_Quarto_de_Despejo_em_1960.jpg/200px-Carolina_Maria_de_Jesus_assinando_seu_livro_Quarto_de_Despejo_em_1960.jpg" />
+    
+          Carolina Maria de Jesus é uma das primeiras autoras negras publicadas no Brasil e teve sua vida atravessada pela miséria e pela fome.
+           Favelada e catadora de papel, narrou em seus escritos a vida dura que teve desde a infância.
+           </div>
+
+           <div>
+      Malcolm X(1925-1965)
+        <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Malcolm_X_NYWTS_2a.jpg/230px-Malcolm_X_NYWTS_2a.jpg" />
+        <p>  Malcolm X foi um ativista que lutou pelos direitos da comunidade afro-americana, durante as décadas de 1950 e 1960, na era dos
+            movimentos dos direitos civis nos Estados Unidos. Adepto do Islamismo, ele fez parte de uma organização afro-americana chamada
+             Nação do Islã entre os anos de 1952 e 1964.</p>
+             </div>
+
+<div>
+      Dandara dos palmares (1654-1694)
+        <img  src="https://lh3.googleusercontent.com/4kgFTOCNDu2jb46BYd9EfIGsLeEWWIdVTvT6q5HOaSKwmcfl3E5dqadgz2latYOWDahaY8C6QNWitb9WhO8Khc13K6XWA1oYvXDrZEP_udOc6uEkdawHQATYAJeNpT4_eza1jFb8" />
+    
+        <p> Dandara, mulher negra e guerreira é um dos principais nomes da luta negra no Brasil. Teve papel fundamental na construção e 
+          comando do quilombo dos Palmares, um dos marcos da resistência contra o regime escravocrata brasileiro, que existiu e resistiu como 
+          quilombo por mais de 100 anos.</p>
+          </div>
+
+<div>
+        Nelson Mandela (1918 – 2013)
+        <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Nelson_Mandela_1994.jpg/220px-Nelson_Mandela_1994.jpg" />
+      
+        <p> Nelson Rolihlahla Mandela foi o principal líder político da história da África do Sul. Também conhecido como Madiba, Mandela lutou
+           contra o regime racista e segregacionista do apartheid, presente em seu país entre 1948 e a década de 1990</p>
+  
+           </div>
+   
+      </main>
+      
     </div>
-    </body>
-  );
+
+    
+  )
 }
